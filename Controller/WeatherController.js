9e73a022accd4fb34be2400ea4db598b942534b7
mehmet <mehmetduvarci@gmail.com>
2019-12-12 20:39:58 +0300

    -A

--- Controller/WeatherController.js
+++ Controller/WeatherController.js
@@ -3,44 +3,16 @@ function getWeather(req, res) {
 
   const city = req.body.city;
 
-
-
-  
-
-    axios
+  axios
     .get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f84e01f0e65d72dcab68d6030065f17a`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f84e01f0e65d72dcab68d6030065f17a`
     )
-    .then(      response => {
+    .then(response => {
       console.log("........", response.data);
-     
+
     })
 
-
-/*     try {
-      return await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f84e01f0e65d72dcab68d6030065f17a`);
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-
-  try {
-    const stats = getStatus(city)
-
-    if (stats) {
-      console.log((stats));
-      res.json({
-        status: "success",
-        data: stats
-      });
-    }
-  }
-  catch (error) {
-    console.error(error)
-  } */
 }
-
 
 module.exports = {
   getWeather
